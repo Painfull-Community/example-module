1ad8584b155fdd4029266f7f10b38b958c34f2d7
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-03 17:30:02 +0200

    copied stuff from discord.js guide, this should work though

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ module.exports = {
         message.channel.send(`Hi, ${message.author}. ${args[0] ? "Cool argument!" : "Try adding an argument!"}`)
     },
     registerEventHandlers: function (cb) {
-        cb("messageReactionAdd", function (message){
-            message.channel.send("Someone reacted!") //please for the love of god dont do this in a real use case
+        cb("messageReactionAdd", async function (reaction, user){
+            if (reaction.partial) {
+                try {
+                    await reaction.fetch();
+                } catch (error) {
+                    console.error('Something went wrong when fetching the message:', error);
+                    return;
+                }
+            }        
+            reaction.message.channel.send("Someone reacted!") //please for the love of god dont do this in a real use case
         })
     }
 }
