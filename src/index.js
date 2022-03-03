6f11cb7dcd9ff4c91a13dabf77f66b69b3cd03b4
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-03 17:17:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ module.exports = {
     execute: function (message, args) {
         message.channel.send(`Hi, ${message.author}. ${args[0] ? "Cool argument!" : "Try adding an argument!"}`)
     },
-    registerEventHandlers: function (bot) {
-        bot.addListener("messageReactionAdd", function (message){
+    registerEventHandlers: function (cb) {
+        cb("messageReactionAdd", function (message){
             message.channel.send("Someone reacted!") //please for the love of god dont do this in a real use case
         })
     }
